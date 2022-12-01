6e7f4ffa33f4e98a6c0f632240184ac6a4254a5c
DSAQWERTY001 <dsaqwerty001@gmail.com>
2022-12-01 20:51:25 +0700

    final test

--- lib/component/encrypt.dart
+++ lib/component/encrypt.dart
@@ -7,10 +7,7 @@ class EncryptData {
     final key = Key.fromUtf8('KMUTNBVOTINGSECRECTUNKNOWKMUTNB1');
     final iv = IV.fromBase16("0000000000000000");
     final encrypter = Encrypter(AES(key, mode: AESMode.ecb));
-    print(AES(key, mode: AESMode.cbc));
-    print(key.base64);
     final encrypted = encrypter.encrypt(plainText, iv: iv);
-    print(encrypted);
     return encrypted;
   }
 }
